727aa68218e1051a4dca4ca29f6f10af12207773
Duaa1986 <doua.abusair@gmail.com>
2021-04-24 00:29:02 +0300

    finalzing CSS & HTML to start with js document and will get back for them for later modifications

---
+++ quiz.js
@@ -0,0 +1,9 @@
+let q1 = "How many days makes a week ?"
+let q2 = "How manay hours can be found in a day ?"
+let q3 = "How many permanent teeth does a dog have ?"
+let q4 = "How many sides does an hexagon have ?"
+let q5 = "How many hearts does an Octopus have ?"
+
+const question = [
+   
+]
